1a25944227cd368b94421ef269ee7d5c8b1f742f
sekichang <sekichang@gmail.com>
2022-05-22 12:33:19 +0900

    add alias

--- config.fish
+++ config.fish
@@ -10,6 +10,9 @@ balias lla 'll -a'
 balias cat bat
 balias g git
 abbr -a ga 'git add'
+abbr -a gco 'git commit'
+abbr -a gch 'git checkout'
+abbr -a gd 'git diff'
 abbr -a gs 'git status'
 balias d docker
 alias vi nvim
