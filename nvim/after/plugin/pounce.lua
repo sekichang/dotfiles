9e46a3f5fa74db673fe581526b0e319e4d8d2fee
sekichang <sekichang@gmail.com>
2022-10-16 04:39:25 +0900

    change color

--- nvim/after/plugin/pounce.lua
+++ nvim/after/plugin/pounce.lua
@@ -11,9 +11,9 @@ nmap S <cmd>PounceRepeat<CR>
 vmap s <cmd>Pounce<CR>
 omap gs <cmd>Pounce<CR>
 
-highlight PounceMatch      cterm=underline,bold ctermfg=gray ctermbg=214 gui=underline,bold guifg=#00ccff guibg=#555555
+highlight PounceMatch      cterm=underline,bold ctermfg=gray ctermbg=214 gui=underline,bold guifg=#ffaa00 guibg=#555555
 highlight PounceGap        cterm=underline,bold ctermfg=gray ctermbg=209 gui=underline,bold guifg=#99ff66 guibg=#333333
-highlight PounceAccept     cterm=underline,bold ctermfg=214 ctermbg=gray gui=underline,bold guifg=#ffaa00 guibg=#333333
-highlight PounceAcceptBest cterm=underline,bold ctermfg=196 ctermbg=gray gui=underline,bold guifg=#ff00fc guibg=#333333
+highlight PounceAccept     cterm=underline,bold ctermfg=214 ctermbg=gray gui=underline,bold guifg=#00ccff guibg=#333333
+highlight PounceAcceptBest cterm=underline,bold ctermfg=196 ctermbg=gray gui=underline,bold guifg=#ff228c guibg=#333333
 highlight PounceUnmatched  ctermfg=248 guifg=#666666
 ]]
