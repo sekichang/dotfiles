4f9a8d6968cbeaaeef58b60695cef7e8c20b8d69
sekichang <sekichang@gmail.com>
2022-10-16 21:48:30 +0900

    Use new default_capabilities

--- nvim/after/plugin/lspconfig.lua
+++ nvim/after/plugin/lspconfig.lua
@@ -29,7 +29,7 @@ end
 
 local lspconfig = require "lspconfig"
 --local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
-local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 local opts = { capabilities = capabilities, on_attach = on_attach }
 
 require("mason").setup()
