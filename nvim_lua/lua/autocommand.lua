71a88bc5a9dec287349b7c0d9f4ea2129a65f3cf
sekichang <sekichang@gmail.com>
2022-11-12 22:23:40 +0900

    remove nvim_lua

--- nvim_lua/lua/autocommand.lua
+++
@@ -1,9 +0,0 @@
-vim.cmd[[autocmd BufWritePre * :%s/\s\+$//ge]]  -- 保存したとき行末の空白削除
-vim.cmd[[
-augroup fileTypeIndent
-  autocmd!
-  autocmd BufNewFile,BufRead *.py setlocal tabstop=4 softtabstop=4 shiftwidth=4
-  autocmd BufNewFile,BufRead *.rb setlocal tabstop=2 softtabstop=2 shiftwidth=2
-augroup END
-]]
-
