3cd73ee9c4d9a21b00f48a5f6e217fddc69da7fa
sekichang <sekichang@gmail.com>
2023-01-14 16:48:52 +0900

    remove line

--- nvim/after/plugin/lspconfig.lua
+++ nvim/after/plugin/lspconfig.lua
@@ -28,7 +28,6 @@ local on_attach = function(client, bufnr)
 end
 
 local lspconfig = require "lspconfig"
---local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 require("mason").setup()
