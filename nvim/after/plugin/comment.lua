fc642194b2badf2931b4fb47bbd90c682dddb198
sekichang <sekichang@gmail.com>
2023-01-14 19:21:37 +0900

    add keymap for comment

--- nvim/after/plugin/comment.lua
+++ nvim/after/plugin/comment.lua
@@ -1,1 +1,4 @@
 require("Comment").setup()
+
+vim.api.nvim_set_keymap("n", "<C-/>", "<Cmd>lua require('Comment.api').toggle.linewise.current()<CR>", {})
+vim.api.nvim_set_keymap("v", "<C-/>", "gc", {})
