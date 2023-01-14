767de356d5d0e3be14073c57879ddae9511b509e
sekichang <sekichang@gmail.com>
2023-01-14 16:31:28 +0900

    update lspsaga

--- nvim/after/plugin/lspsaga.lua
+++ nvim/after/plugin/lspsaga.lua
@@ -2,26 +2,115 @@ local saga = require "lspsaga"
 local keymap = vim.keymap.set
 local diagnostic = vim.diagnostic
 
--- use custom config
-saga.init_lsp_saga {
-  code_action_icon = "",
-  rename_action_quit = "<ESC>",
-  finder_action_keys = {
+saga.setup {
+  finder = {
     open = { "o", "<CR>" },
-    vsplit = "s",
+    vsplit = "v",
     split = "i",
     tabe = "t",
     quit = { "q", "<ESC>" },
   },
-  code_action_keys = {
+  definition = {
+    edit = "<C-c>o",
+    vsplit = "<C-c>v",
+    split = "<C-c>i",
+    tabe = "<C-c>t",
     quit = "q",
-    exec = "<CR>",
+    close = "<Esc>",
   },
-  definition_action_keys = {
-    edit = "<CR>",
-    vsplit = "s",
-    tabe = "t",
-    quit = "q",
+  code_action = {
+    num_shortcut = true,
+    keys = {
+      quit = "q",
+      exec = "<CR>",
+    },
+  },
+  lightbulb = {
+    enable = true,
+    enable_in_insert = true,
+    sign = true,
+    sign_priority = 40,
+    virtual_text = true,
+  },
+  diagnostic = {
+    twice_into = false,
+    show_code_action = true,
+    show_source = true,
+    keys = {
+      exec_action = "o",
+      quit = "q",
+      go_action = "g",
+    },
+  },
+  rename = {
+    quit = "<ESC>",
+    exec = "<CR>",
+    in_select = true,
+  },
+  outline = {
+    win_position = "right",
+    win_with = "",
+    win_width = 30,
+    show_detail = true,
+    auto_preview = true,
+    auto_refresh = true,
+    auto_close = true,
+    custom_sort = nil,
+    keys = {
+      jump = "o",
+      expand_collapse = "u",
+      quit = "q",
+    },
+  },
+  callhierarchy = {
+    show_detail = false,
+    keys = {
+      edit = "e",
+      vsplit = "v",
+      split = "i",
+      tabe = "t",
+      jump = "o",
+      quit = "q",
+      expand_collapse = "u",
+    },
+  },
+  symbol_in_winbar = {
+    enable = false,
+    separator = " ",
+    hide_keyword = true,
+    show_file = true,
+    folder_level = 2,
+  },
+  ui = {
+    -- currently only round theme
+    theme = "round",
+    -- border type can be single,double,rounded,solid,shadow.
+    border = "single",
+    winblend = 0,
+    expand = "",
+    collapse = "",
+    preview = " ",
+    code_action = "💡",
+    diagnostic = "🐞",
+    incoming = " ",
+    outgoing = " ",
+    colors = {
+      --float window normal bakcground color
+      normal_bg = "#1d2536",
+      --title background color
+      title_bg = "#afd700",
+      red = "#e95678",
+      magenta = "#b33076",
+      orange = "#FF8700",
+      yellow = "#f7bb3b",
+      green = "#afd700",
+      cyan = "#36d0e0",
+      blue = "#61afef",
+      purple = "#CBA6F7",
+      white = "#d1d4cf",
+      black = "#1c1c19",
+    },
+    kind = {},
   },
 }
 
@@ -31,19 +120,20 @@ local opts = { silent = true }
 keymap("n", "gh", "<cmd>Lspsaga lsp_finder<CR>", opts)
 
 -- Code action
-keymap({ "n", "v" }, "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts)
+keymap({ "n", "v" }, "<space>ca", "<cmd>Lspsaga code_action<CR>", opts)
 
 -- Rename
 keymap("n", "gr", "<cmd>Lspsaga rename<CR>", opts)
 
 -- Definition preview
+-- support tagstack C-t jump back
 keymap("n", "gd", "<cmd>Lspsaga peek_definition<CR>", opts)
 
 -- Show line diagnostics
-keymap("n", "<leader>cd", "<cmd>Lspsaga show_line_diagnostics<CR>", opts)
+--keymap("n", "<space>sl", "<cmd>Lspsaga show_line_diagnostics<CR>", opts)
 
 -- Show cursor diagnostic
---keymap("n", "<leader>cd", "<cmd>Lspsaga show_cursor_diagnostics<CR>", opts)
+--keymap("n", "<space>sc", "<cmd>Lspsaga show_cursor_diagnostics<CR>", opts)
 
 -- Diagnsotic jump can use `<c-o>` to jump back
 keymap("n", "[e", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts)
@@ -58,7 +148,11 @@ keymap("n", "]E", function()
 end, opts)
 
 -- Outline
-keymap("n", "<leader>o", "<cmd>LSoutlineToggle<CR>", opts)
+keymap("n", "<space>o", "<cmd>Lspsaga outline<CR>", opts)
 
 -- Hover Doc
 keymap("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts)
+
+-- Callhierarchy
+keymap("n", "<space>ci", "<cmd>Lspsaga incoming_calls<CR>", opts)
+keymap("n", "<space>co", "<cmd>Lspsaga outgoing_calls<CR>", opts)
