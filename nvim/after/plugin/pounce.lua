a69938840c294183944a64813fa9452f9066629f
sekichang <sekichang@gmail.com>
2023-03-05 13:51:11 +0900

    update pounce

--- nvim/after/plugin/pounce.lua
+++ nvim/after/plugin/pounce.lua
@@ -10,9 +10,9 @@ vim.keymap.set("v", "s", "<cmd>Pounce<CR>")
 vim.keymap.set("o", "gs", "<cmd>Pounce<CR>")
 
 vim.cmd [[
-highlight PounceMatch      cterm=underline,bold ctermfg=gray ctermbg=214 gui=underline,bold guifg=#ffaa00 guibg=#555555
-highlight PounceGap        cterm=underline,bold ctermfg=gray ctermbg=209 gui=underline,bold guifg=#99ff66 guibg=#333333
-highlight PounceAccept     cterm=underline,bold ctermfg=214 ctermbg=gray gui=underline,bold guifg=#00ccff guibg=#333333
-highlight PounceAcceptBest cterm=underline,bold ctermfg=196 ctermbg=gray gui=underline,bold guifg=#ff228c guibg=#333333
-highlight PounceUnmatched  ctermfg=248 guifg=#666666
+" highlight PounceMatch      cterm=underline,bold ctermfg=gray ctermbg=214 gui=underline,bold guifg=#ffaa00 guibg=#555555
+" highlight PounceGap        cterm=underline,bold ctermfg=gray ctermbg=209 gui=underline,bold guifg=#99ff66 guibg=#333333
+ highlight PounceAccept     cterm=underline,bold ctermfg=214 ctermbg=gray gui=underline,bold guifg=#66ccff guibg=#333333
+ highlight PounceAcceptBest cterm=underline,bold ctermfg=196 ctermbg=gray gui=underline,bold guifg=#ff448c guibg=#333333
+ highlight PounceUnmatched  ctermfg=248 guifg=#666666
 ]]
