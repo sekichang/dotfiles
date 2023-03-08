5d5161917d8bba2e0870700cd2b64b3dba78e253
sekichang <sekichang@gmail.com>
2023-03-08 23:02:48 +0900

    add keymap(bufferline)

--- nvim/after/plugin/bufferline.lua
+++ nvim/after/plugin/bufferline.lua
@@ -29,3 +29,5 @@ require("bufferline").setup {
     --   },
   },
 }
+vim.keymap.set("n", "<Tab>", "<Cmd>BufferLineCycleNext<CR>", {})
+vim.keymap.set("n", "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", {})
