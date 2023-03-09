f834f077b606b6766300b04d77b65e2f147ca849
sekichang <sekichang@gmail.com>
2023-03-10 05:20:09 +0900

    Enabled toggleterm

--- nvim/after/plugin/which-key.lua
+++ nvim/after/plugin/which-key.lua
@@ -157,16 +157,12 @@ local mappings = {
     r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File" },
     R = { "<cmd>Telescope registers<cr>", "Registers" },
   },
-  -- t = {
-  --   name = "Terminal",
-  --   --    n = { "<cmd>lua _NODE_TOGGLE()<cr>", "Node" },
-  --   --    u = { "<cmd>lua _NCDU_TOGGLE()<cr>", "NCDU" },
-  --   --    t = { "<cmd>lua _HTOP_TOGGLE()<cr>", "Htop" },
-  --   --    p = { "<cmd>lua _PYTHON_TOGGLE()<cr>", "Python" },
-  --   f = { "<cmd>ToggleTerm direction=float<cr>", "Float" },
-  --   h = { "<cmd>ToggleTerm size=10 direction=horizontal<cr>", "Horizontal" },
-  --   v = { "<cmd>ToggleTerm size=80 direction=vertical<cr>", "Vertical" },
-  -- },
+  t = {
+    name = "Terminal",
+    f = { "<cmd>ToggleTerm direction=float<cr>", "Float" },
+    h = { "<cmd>ToggleTerm size=10 direction=horizontal<cr>", "Horizontal" },
+    v = { "<cmd>ToggleTerm size=80 direction=vertical<cr>", "Vertical" },
+  },
 }
 
 which_key.setup(setup)
