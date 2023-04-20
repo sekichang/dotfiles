46a7730223e6a3f4a7dcce4cf9516f443d01ce9d
sekichang <sekichang@gmail.com>
2023-04-20 11:04:25 +0900

    Change percent output conditions

--- tmux/get_battery.sh
+++ tmux/get_battery.sh
@@ -3,11 +3,13 @@
 if battery_info=$(/usr/bin/pmset -g ps | awk '{ if (NR == 2) print $3 " " $4 }' | sed -e "s/;//g" -e "s/%//") ; then
   battery_quantity=$(echo $battery_info | awk '{print $1}')
   if [[ ! $battery_info =~ "discharging" ]]; then
-    battery="#[bg=cyan,fg=black] ⚡ $battery_quantity% #[default]"
-  elif (( $battery_quantity < 16 )); then
-    battery="#[bg=red,fg=white] $battery_quantity% #[default]"
+    battery="#[bg=green,fg=black]  ⚡$battery_quantity% #[default]"
+  elif (( $battery_quantity > 20 )); then
+    battery="#[bg=green,fg=black] $battery_quantity% #[default]"
+  elif (( $battery_quantity > 5 )); then
+    battery="#[bg=yellow,fg=black] $battery_quantity% #[default]"
   else
-    battery="#[bg=cyan,fg=black] $battery_quantity% #[default]"
+    battery="#[bg=red,fg=black] $battery_quantity% #[default]"
   fi
   echo $battery
 fi
