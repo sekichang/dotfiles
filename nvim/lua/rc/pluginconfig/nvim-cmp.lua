2e02ad01953787c35e56ab0ca93676317839693f
sekichang <sekichang@gmail.com>
2023-09-15 01:40:14 +0900

    Update cmp

--- nvim/lua/rc/pluginconfig/nvim-cmp.lua
+++ nvim/lua/rc/pluginconfig/nvim-cmp.lua
@@ -93,9 +93,11 @@ cmp.setup.filetype("gitcommit", {
 -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline("/", {
   mapping = cmp.mapping.preset.cmdline(),
-  sources = {
+  sources = cmp.config.sources({
+    { name = "nvim_lsp_document_symbol" },
+  }, {
     { name = "buffer" },
-  },
+  }),
 })
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
