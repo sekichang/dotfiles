372ef1b24381ac5679dfdc71fc309da607304a5c
sekichang <sekichang@gmail.com>
2023-10-10 22:27:04 +0900

    Update indent-blankline config

--- nvim/lua/rc/pluginconfig/indent-blankline.lua
+++ nvim/lua/rc/pluginconfig/indent-blankline.lua
@@ -1,26 +1,31 @@
-require("indent_blankline").setup({
-  show_first_indent_level = false,
+local hooks = require("ibl.hooks")
+hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
+
+require("ibl").overwrite({
+
   -- 縦線を除外するプラグイン等
-  filetype_exclude = {
-    "help",
-    "dashboard",
-    "dashpreview",
-    --"NvimTree",
-    --"neo-tree",
-    --"vista",
-    "sagahover",
-    "sagasignature",
-    "lazy",
-    "log",
-    "lspsagafinder",
-    "lspinfo",
-    --"dapui_scopes",
-    --"dapui_breakpoints",
-    --"dapui_stacks",
-    --"dapui_watches",
-    --"dap-repl",
-    "toggleterm",
-    "alpha",
-    --"coc-explorer",
+  exclude = {
+    filetypes = {
+      "help",
+      "dashboard",
+      "dashpreview",
+      --"NvimTree",
+      --"neo-tree",
+      --"vista",
+      "sagahover",
+      "sagasignature",
+      "lazy",
+      "log",
+      "lspsagafinder",
+      "lspinfo",
+      --"dapui_scopes",
+      --"dapui_breakpoints",
+      --"dapui_stacks",
+      --"dapui_watches",
+      --"dap-repl",
+      "toggleterm",
+      "alpha",
+      --"coc-explorer",
+    },
   },
 })
