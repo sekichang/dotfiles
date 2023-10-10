a8937728c6f6471907f4a8b84199488d05d56162
sekichang <sekichang@gmail.com>
2023-10-10 22:28:06 +0900

    Add cmp-cmdline

--- nvim/lua/rc/plugins.lua
+++ nvim/lua/rc/plugins.lua
@@ -78,6 +78,7 @@ require("lazy").setup({
       { "hrsh7th/cmp-path" },
       { "hrsh7th/cmp-nvim-lua" },
       { "hrsh7th/cmp-calc" },
+      { "hrsh7th/cmp-cmdline" },
       { "saadparwaiz1/cmp_luasnip" },
       { "onsails/lspkind-nvim" },
       ---- Snippet
