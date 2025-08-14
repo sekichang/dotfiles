64de891d177f1e2f83ea45817acffdd6c67aefc1
sekichang <sekichang@gmail.com>
2025-08-15 00:19:12 +0900

    Add render-markdown.nvim

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -66,4 +66,24 @@ return {
       },
     },
   },
+
+  -- markdown
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    opts = {
+      code = {
+        sign = false,
+        width = "block",
+        right_pad = 1,
+        border = "thick",
+      },
+      heading = {
+        sign = false,
+        icons = {},
+      },
+      checkbox = {
+        enabled = true,
+      },
+    },
+  },
 }
