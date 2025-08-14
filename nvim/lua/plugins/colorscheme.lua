c25a848dc6fb3ac0406592ad43d9f85ec5ef9168
sekichang <sekichang@gmail.com>
2025-08-15 00:21:48 +0900

    remove theme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -13,18 +13,7 @@ return {
       },
     },
   },
-  --
-  -- {
-  --   "craftzdog/solarized-osaka.nvim",
-  --   lazy = true,
-  --   priority = 1000,
-  --   opts = function()
-  --     return {
-  --       transparent = true,
-  --     }
-  --   end,
-  -- },
-  --
+
   {
     "EdenEast/nightfox.nvim",
     lazy = true,
