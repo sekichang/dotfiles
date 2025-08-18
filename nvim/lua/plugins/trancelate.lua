6c6428d62b21de505ff5f1f415c812ecab08b44c
sekichang <sekichang@gmail.com>
2025-08-18 16:10:26 +0900

    update keymap

--- nvim/lua/plugins/trancelate.lua
+++ nvim/lua/plugins/trancelate.lua
@@ -2,7 +2,7 @@ return {
   {
     "uga-rosa/translate.nvim",
     keys = {
-      { "<leader>t", "<cmd>Translate ja<cr>", desc = "Translate" },
+      { "<leader>t", "<cmd>Translate ja<cr>", desc = "Translate", mode = { "n", "v" } },
     },
   },
 }
