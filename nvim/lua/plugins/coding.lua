f2040d3ecef3b3a6e4faabd3735cb1bbd5a4cc74
sekichang <sekichang@gmail.com>
2025-08-30 01:43:41 +0900

    remove cmp-emoji

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -5,13 +5,6 @@ return {
     cmd = "IncRename",
     config = true,
   },
-  -- {
-  --   "nvim-cmp",
-  --   dependencies = { "hrsh7th/cmp-emoji" },
-  --   opts = function(_, opts)
-  --     table.insert(opts.sources, { name = "emoji" })
-  --   end,
-  -- },
   {
     "saghen/blink.cmp",
     version = not vim.g.lazyvim_blink_main and "*",
