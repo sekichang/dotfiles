0ec220a15cd1432dd6afd2ac7f2b975ddd0467e3
sekichang <sekichang@gmail.com>
2026-03-29 18:01:35 +0900

    更新日時とgit statusを表示する

--- yazi/init.lua
+++ yazi/init.lua
@@ -1,5 +1,14 @@
 require("git"):setup({
-	order = 1500,
+	-- order = 1500,
+	order = 0,
 })
 
 require("zoxide"):setup()
+
+---@diagnostic disable: undefined-global
+function Linemode:mtime_only()
+	local time = math.floor(self._file.cha.mtime or 0)
+	local mtime = time ~= 0 and os.date("%Y/%m/%d %H:%M", time) or ""
+	-- return ui.Line(string.format("%-17s", mtime))
+	return mtime
+end
