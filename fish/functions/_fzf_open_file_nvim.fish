83504b7d534fb5ad183746061d9db15b8fa0479c
sekichang <sekichang@gmail.com>
2026-04-04 06:42:55 +0900

    空白行を削除

--- fish/functions/_fzf_open_file_nvim.fish
+++ fish/functions/_fzf_open_file_nvim.fish
@@ -1,4 +1,3 @@
-
 function _fzf_open_file_nvim
     set -l selected (fd --type f --hidden --exclude .git | fzf --prompt "open file ❯ " --height 40% --layout reverse --border --preview "bat --color=always {}")
 
@@ -7,4 +6,3 @@ function _fzf_open_file_nvim
         commandline -f repaint
     end
 end
-
