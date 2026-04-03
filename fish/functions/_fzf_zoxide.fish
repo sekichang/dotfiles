9f9814223837a84f6dcc22661c9b07ff76cfcae0
sekichang <sekichang@gmail.com>
2026-04-04 06:57:49 +0900

    ディレクトリの中をezaで表示するようにした

--- fish/functions/_fzf_zoxide.fish
+++ fish/functions/_fzf_zoxide.fish
@@ -1,11 +1,19 @@
 function _fzf_zoxide
-    set -l dir (begin
-        ghq list --full-path | awk '{printf "       - [ghq] %s\n", $0}'
-        zoxide query --list --score | awk '{printf "%7.2f  [zi]  %s\n", $1, $2}'
-    end | fzf --height 40% --reverse --prompt "zoxide> " \
-        --ansi \
-        | awk '{print $NF}')
-
+    set -l dir (
+        begin
+            ghq list --full-path | awk '{printf "       - [ghq] %s\n", $0}'
+            zoxide query --list --score | awk '{printf "%7.2f  [zi]  %s\n", $1, $2}'
+        end | fzf \
+            --reverse \
+            --style=full:rounded \
+            --height=75% \
+            --margin=0,5% \
+            --preview-window=down,50%,wrap \
+            --preview 'eza -l --icons --sort modified -r --color=always --time-style iso {-1}' \
+            --no-sort \
+            --exact \
+        | awk '{print $NF}'
+    )
     if test -n "$dir"
         cd $dir
         zoxide add $dir
