1b5ffbd7724a172502c59c7c9becff58b86bd5bc
sekichang <sekichang@gmail.com>
2026-04-04 07:02:12 +0900

    デフォルト値の変更

--- fish/conf.d/fzf.fish
+++ fish/conf.d/fzf.fish
@@ -1,9 +1,9 @@
 set -x FZF_DEFAULT_COMMAND 'fd --type f --hidden --follow --exclude .git'
 set -x FZF_DEFAULT_OPTS '
-  --height 40%
   --layout reverse
-  --border rounded
-  --bind ctrl-u:preview-page-up,ctrl-d:preview-page-down
+  --style=full:rounded
+  --height 45%
+  --margin 0,5%
 '
 set -x FZF_DEFAULT_OPTS "$FZF_DEFAULT_OPTS
   --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8
@@ -11,14 +11,14 @@ set -x FZF_DEFAULT_OPTS "$FZF_DEFAULT_OPTS
   --color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8
   --color=selected-bg:#45475a
 "
-set -x FZF_CTRL_T_COMMAND 'fd --type f --hidden --follow --exclude .git'
+set -x FZF_CTRL_T_COMMAND 'fd --type f --hidden --follow --exclude .git --exclude node_modules'
 set -x FZF_CTRL_T_OPTS '
-  --preview "if test -d {}; eza --tree --level=2 --color=always {}; else; bat --color=always --style=numbers {}; end"
-  --preview-window right:50%
+  --preview "bat --color=always --style=numbers {}"
+  --preview-window right:60%
 '
 set -x FZF_CTRL_R_OPTS '
   --with-nth 2..
-  --preview "echo {}"
+  --preview "echo {2..}"
   --preview-window down:3:wrap
   --bind "ctrl-y:execute-silent(echo -n {2..} | pbcopy)"
 '
