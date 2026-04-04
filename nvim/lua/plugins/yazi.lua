5a0841c10f82ebac03c1bf621d0ca48691c0e677
sekichang <sekichang@gmail.com>
2026-04-04 16:33:48 +0900

    <leader>feを元に戻す

--- nvim/lua/plugins/yazi.lua
+++ nvim/lua/plugins/yazi.lua
@@ -4,6 +4,5 @@ return {
   dependencies = { "nvim-lua/plenary.nvim" },
   keys = {
     { "<leader>e", "<cmd>Yazi<cr>", desc = "Open Yazi" },
-    { "<leader>fe", "<cmd>Yazi<cr>", desc = "Open Yazi" },
   },
 }
