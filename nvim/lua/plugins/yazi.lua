e1e20db9c8301d4b05dfb4f6b57a7607f30cde8a
sekichang <sekichang@gmail.com>
2026-04-04 11:31:39 +0900

    起動タイミングを変更するのとyazi起動ショートカット変更

--- nvim/lua/plugins/yazi.lua
+++ nvim/lua/plugins/yazi.lua
@@ -1,7 +1,9 @@
 return {
   "mikavilpas/yazi.nvim",
+  event = "VeryLazy",
   dependencies = { "nvim-lua/plenary.nvim" },
   keys = {
     { "<leader>e", "<cmd>Yazi<cr>", desc = "Open Yazi" },
+    { "<leader>fe", "<cmd>Yazi<cr>", desc = "Open Yazi" },
   },
 }
