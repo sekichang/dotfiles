9b17db1a14a03068aa906eff5a270ca9e68c8f80
sekichang <sekichang@gmail.com>
2026-04-04 11:35:21 +0900

    表示の仕方をziコマンドと同じになるように変更

--- fish/functions/y.fish
+++ fish/functions/y.fish
@@ -1,6 +1,11 @@
 function y
+    set _FZF_OPTS "$FZF_DEFAULT_OPTS
+--height 90%
+--preview 'bat --style=numbers --color=always {}'
+--preview-window=right,50%,wrap"
+
     set tmp (mktemp -t "yazi-cwd.XXXXXX")
-    command yazi $argv --cwd-file="$tmp"
+    FZF_DEFAULT_OPTS="$_FZF_OPTS" command yazi $argv --cwd-file="$tmp"
     if read -z cwd <"$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
         builtin cd -- "$cwd"
     end
