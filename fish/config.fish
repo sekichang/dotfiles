8aac48f77f270867f393382b8c0d02b54e5031ad
sekichang <sekichang@gmail.com>
2026-04-05 19:32:28 +0900

    コメント修正のみ

--- fish/config.fish
+++ fish/config.fish
@@ -2,7 +2,7 @@
 # General
 # =============================================================================
 
-# config.fish
+# テーマ
 if status is-interactive
     fish_config theme choose catppuccin-mocha --color-theme=dark
 end
