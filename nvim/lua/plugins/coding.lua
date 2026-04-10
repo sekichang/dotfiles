fccf0a9ec4362efa366f51881148098b989f741b
sekichang <sekichang@gmail.com>
2026-04-11 03:28:23 +0900

    mini.surroundの追加

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -151,4 +151,18 @@ return {
       require("blink.cmp").setup(opts)
     end,
   },
+  {
+    "nvim-mini/mini.surround",
+    opts = {
+      mappings = {
+        add = "sa",
+        delete = "sd",
+        find = "gsf",
+        find_left = "gsF",
+        highlight = "gsh",
+        replace = "sr",
+        update_n_lines = "gsn",
+      },
+    },
+  },
 }
