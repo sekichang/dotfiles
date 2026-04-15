33bee19df5793d0e815f0d361387a3bc8bf449a9
sekichang <sekichang@gmail.com>
2026-04-16 05:47:39 +0900

    yazi.nvimをeditor.luaに移動

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -41,4 +41,12 @@ return {
       },
     },
   },
+  {
+    "mikavilpas/yazi.nvim",
+    event = "VeryLazy",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    keys = {
+      { "<leader>e", "<cmd>Yazi<cr>", desc = "Open Yazi" },
+    },
+  },
 }
--- nvim/lua/plugins/yazi.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "mikavilpas/yazi.nvim",
-  event = "VeryLazy",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  keys = {
-    { "<leader>e", "<cmd>Yazi<cr>", desc = "Open Yazi" },
-  },
-}
