92e2674cf69d62dfec951a18359c6cdd9799d3c9
sekichang <sekichang@gmail.com>
2026-04-19 09:40:53 +0900

    keymap更新

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,23 +2,41 @@
 local discipline = require("util.discipline")
 discipline.cowboy()
 
-local keymap = vim.keymap.set
+local map = vim.keymap.set
 
-keymap({ "n", "x" }, "x", '"_x', { desc = "Delete without affecting the registers" })
-keymap({ "n", "x" }, "c", '"_c', { desc = "Change without affecting the registers" })
+-- 削除/変更でレジスタを使わない
+map({ "n", "x" }, "x", '"_x')
+map({ "n", "x" }, "c", '"_c')
 
-keymap("n", "<C-a>", "ggVG", { desc = "Select all" })
-keymap({ "n", "x" }, "H", "^", { desc = "line" })
-keymap({ "n", "x" }, "L", "$", { desc = "End of line" })
-keymap("n", "G", "Gzz", { desc = "Go to end of file" })
-keymap("n", "n", "nzz", { desc = "Next search result" })
-keymap("n", "N", "Nzz", { desc = "Prev search result" })
-keymap("n", "<c-d>", "<c-d>zz", { desc = "Scroll down half page" })
-keymap("n", "<c-u>", "<c-u>zz", { desc = "Scroll up half page" })
-keymap("n", "+", "<C-a>", { desc = "Increment" })
-keymap("n", "-", "<C-x>", { desc = "Decrement" })
-keymap({ "n", "i", "v" }, "<C-s>", "<cmd>silent w<cr><esc>", { desc = "Save File" })
+-- 行頭/行末へ移動
+map({ "n", "x" }, "H", "^")
+map({ "n", "x" }, "L", "$")
+
+-- ファイルの末尾へ移動
+map("n", "G", "Gzz")
+
+-- 検索結果の次/前
+map("n", "n", "nzz")
+map("n", "N", "Nzz")
+
+-- 半ページスクロール
+map("n", "<c-d>", "<c-d>zz")
+map("n", "<c-u>", "<c-u>zz")
+
+-- increment/decrement
+map("n", "+", "<C-a>")
+map("n", "-", "<C-x>")
+
+-- 全選択
+map("n", "<C-a>", "ggVG")
+
+-- ファイル保存
+map("n", "<C-s>", "<cmd>silent w<cr><esc>")
+
+-- マクロ記録をQに退避
+map("n", "q", "<nop>")
+map("n", "Q", "q", { noremap = true })
 
 -- Insert mode cursor movement
-keymap("i", "<C-b>", "<Left>", { desc = "Move cursor left" })
-keymap("i", "<C-f>", "<Right>", { desc = "Move cursor right" })
+map("i", "<C-b>", "<Left>")
+map("i", "<C-f>", "<Right>")
