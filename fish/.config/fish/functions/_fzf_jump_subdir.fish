bc5384e01cdb307032b05a22a3107495fc84b7af
sekichang <sekichang@gmail.com>
2026-05-06 12:39:57 +0900

    隠しフォルダも表示対象にする
    ただし.gitは対象外にする
    ついでにnode_modulesも対象外にする

--- fish/.config/fish/functions/_fzf_jump_subdir.fish
+++ fish/.config/fish/functions/_fzf_jump_subdir.fish
@@ -1,5 +1,5 @@
 function _fzf_jump_subdir
-    set -l selected (fd --type d --max-depth 3 . $PWD | fzf --prompt="jump to subdir> " --height 40% --layout reverse --border)
+    set -l selected (fd --type d --hidden --max-depth 3 --exclude .git --exclude node_modules . $PWD | fzf --prompt="jump to subdir> " --height 40% --layout reverse --border)
     if test -n "$selected"
         cd $selected
         commandline -f repaint
