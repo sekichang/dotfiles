e652a702fae7a6f40f7948327d5ed5e301a890b6
sekichang <sekichang@gmail.com>
2026-05-10 18:28:42 +0900

    previewを追加

--- fish/.config/fish/functions/_fzf_jump_parent_dirs.fish
+++ fish/.config/fish/functions/_fzf_jump_parent_dirs.fish
@@ -7,7 +7,13 @@ function _fzf_jump_parent_dirs
         set -a dirs $dir
     end
 
-    set -l selected (string join \n $dirs | fzf --prompt "jump to parent ❯ " --height 40% --layout reverse --border)
+    set -l selected (string join \n $dirs | fzf \
+      --prompt "jump to parent ❯ " \
+      --height 80% \
+      --layout reverse \
+      --border \
+      --preview-window=down,50%,wrap \
+      --preview 'eza -l --icons --sort modified -r --color=always --time-style iso {-1}')
 
     if test -n "$selected"
         cd $selected
--- fish/.config/fish/functions/_fzf_jump_subdir.fish
+++ fish/.config/fish/functions/_fzf_jump_subdir.fish
@@ -1,5 +1,11 @@
 function _fzf_jump_subdir
-    set -l selected (fd --type d --hidden --max-depth 3 --exclude .git --exclude node_modules . $PWD | fzf --prompt="jump to subdir> " --height 40% --layout reverse --border)
+    set -l selected (fd --type d --hidden --max-depth 3 --exclude .git --exclude node_modules . $PWD | fzf \
+      --prompt="jump to subdir> " \
+      --height 80% \
+      --layout reverse \
+      --border \
+      --preview-window=down,50%,wrap \
+      --preview 'eza -l --icons --sort modified -r --color=always --time-style iso {-1}')
     if test -n "$selected"
         cd $selected
         commandline -f repaint
