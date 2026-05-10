670e0781c904a8b3856b7f200e5181e2cd0c89ea
sekichang <sekichang@gmail.com>
2026-05-10 20:57:20 +0900

    未使用な関数のため削除

--- fish/.config/fish/functions/_fzf_history.fish
+++
@@ -1,8 +0,0 @@
-function _fzf_history
-    set -l selected (builtin history | sort | uniq -c | sort -rn | awk '{$1=""; sub(/^ /, ""); print}' | fzf --prompt "history ❯ " --height 40% --layout reverse --border --no-sort)
-
-    if test -n "$selected"
-        commandline $selected
-        commandline -f repaint
-    end
-end
