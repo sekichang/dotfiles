c274c67e21bb89a574dc7e9e0bf0ef8fcf8d33a8
sekichang <sekichang@gmail.com>
2026-05-10 20:57:39 +0900

    コメント行削除

--- fish/.config/fish/functions/fish_user_key_bindings.fish
+++ fish/.config/fish/functions/fish_user_key_bindings.fish
@@ -2,7 +2,6 @@ function fish_user_key_bindings
     fzf --fish | source
 
     # Ctrl+J — zoxide interactive (zi)
-    # bind \cj 'zi; commandline -f repaint'
     bind \cj _fzf_zoxide
 
     # Ctrl+O - 直近のファイルを選択してnvimでopen
@@ -13,10 +12,8 @@ function fish_user_key_bindings
 
     # Ctrl+, - 親ディレクトリ以上の祖先をfzfで選択してjump
     bind \e\[44\;5u _fzf_jump_parent_dirs
-    # bind \ej _fzf_jump_parent_dirs
 
     # Ctrl+. - サブディレクトリをfzfで選択してjump
     bind \e\[46\;5u _fzf_jump_subdir
-    # bind \ek _fzf_jump_subdir
 
 end
